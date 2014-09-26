bea409a9ce91a8c1a66886912ab35ba71489445c
myeung58 <myeung58gmail.com>
2014-09-26 16:17:40 -0700

    index for jobthings controller

--- app/controllers/jobthings_controller.rb
+++ app/controllers/jobthings_controller.rb
@@ -1,6 +1,11 @@
 class JobthingsController < ApplicationController
 
   def index
+    # substitute params[:user_id] with session[:user_id] once session model is ready
+    @jobthings = Jobthing.where(user_id: params[:user_id])
   end
+
+  private
+
 
 end
