068449ac83ebf24fb2bd95ddde045f5df78584aa
Alisa Tatarinova <alisatatarinova@gmail.com>
2014-09-26 11:57:48 -0700

    Fixed indentation in both models and schemas

--- db/migrate/20140926180833_create_jobthings.rb
+++ db/migrate/20140926180833_create_jobthings.rb
@@ -1,11 +1,11 @@
 class CreateJobthings < ActiveRecord::Migration
   def change
     create_table :jobthings do |t|
-	  	t.string :job_link
-		t.string :company
-		t.string :position 
-		t.belongs_to :user
-		t.timestamps
+		  t.string :job_link
+			t.string :company
+			t.string :position 
+			t.belongs_to :user
+			t.timestamps
     end
   end
 end
