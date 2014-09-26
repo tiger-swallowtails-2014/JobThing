5a683d54fcd5bff7c6b5907323b2d71955e7bc56
myeung58 <myeung58gmail.com>
2014-09-26 16:21:24 -0700

    implemented new and create methods

--- app/controllers/jobthings_controller.rb
+++ app/controllers/jobthings_controller.rb
@@ -1,11 +1,25 @@
 class JobthingsController < ApplicationController
 
   def index
-    # substitute params[:user_id] with session[:user_id] once session model is ready
-    @jobthings = Jobthing.where(user_id: params[:user_id])
+    load_user
+    @jobthings = Jobthing.where(user_id: @user.id)
+  end
+
+  def new
+    @jobthing = Jobthing.new
+  end
+
+  def create
+    load_user
+    @jobthing = Jobthing.create(job_link: params[:job_link], company: params[:company], position: params[:position])
+    @user.jobthings << @jobthing
   end
 
   private
 
+  def load_user
+    # substitute params[:user_id] with session[:user_id] once session model is ready
+    @user = User.find(params[:user_id])
+  end
 
 end
