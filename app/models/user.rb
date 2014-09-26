4779029cdb418b9979b147ad90b7028222627ac2
William Pangestu <pangestu.william@gmail.com>
2014-09-26 16:42:40 -0700

    modify user model for bcrypt implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
+  attr_accesible :email, :password, :password_confirmation
   validates_uniqueness_of :username, :email
-  validates_presence_of :username, :password_hash, :email
+  validates_presence_of :username, :email
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_presence_of :password, :on => :create
 
+  has_secure_password
 	has_many :jobthings
 	has_many :notes
+
+
 end
