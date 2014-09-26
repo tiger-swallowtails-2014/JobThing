e256195a31979d9f73abd773defc5cd063baa42e
myeung58 <myeung58gmail.com>
2014-09-26 15:21:18 -0700

    created seed file and fixed indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
 
 	has_many :jobthings
 	has_many :notes
-	has_many :notes, through: :jobthings
+	# has_many :notes, through: :jobthings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create(
+  first_name: "fname",
+  last_name: "lname",
+  email: "test@test.com",
+  username: "test",
+  password_hash: "test"
+)
+
+contact = Contact.create(
+  first_name: "fname1",
+  last_name: "lname1",
+  phone_number: "1231231234",
+  email: "contact1@contact.com"
+)
+
+note = Note.create(
+  note: "I fucked up",
+  phase_column: "interview",
+)
+
+jobthing = Jobthing.create(
+  job_link: "http://www.google.com",
+  company: "Google",
+  position: "janitor"
+)
+
+user.jobthings << jobthing
+user.notes << note
+
+jobthing.applied = Applied.create()
+jobthing.interview = Interview.create(interview_with: "interviewer", interview_date: "October 1 2014", interview_time: "12 pm")
+jobthing.miscjobthing = Miscjobthing.create(name: "coffee")
+jobthing.outcome = Outcome.create()
+
+jobthing.contacts << contact
+jobthing.notes << note
+
+
+
