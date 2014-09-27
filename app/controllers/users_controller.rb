9393398dd6dacf9b1842384196c9452ad8d9a2da
William Pangestu <pangestu.william@gmail.com>
2014-09-26 17:04:04 -0700

    mofify user controller for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.create(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path
+    else
+      redirect_to root
   end
 
 end
