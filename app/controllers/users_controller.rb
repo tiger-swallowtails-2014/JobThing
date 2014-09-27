8c59e34f891b6fbb85ec2b4b5f6fb718d993763a
William Pangestu <pangestu.william@gmail.com>
2014-09-26 17:51:11 -0700

    Successfully implement user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,27 @@
 class UsersController < ApplicationController
+  protect_from_forgery
 
   def index
-    # @
-    # @jobs = Jobs.all
+    @user = User.new
   end
 
   def create
-    @user = User.create(params[:user])
+    @user = User.create(person_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path
+      redirect_to user_path(@user)
     else
-      redirect_to root
+      redirect_to root_path
+    end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def person_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accesible :email, :password, :password_confirmation
+  # attr_accessible :email, :password, :password_confirmation
   validates_uniqueness_of :username, :email
   validates_presence_of :username, :email
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates_presence_of :password, :on => :create
+  # validates_presence_of :password, :on => :create
 
   has_secure_password
 	has_many :jobthings
