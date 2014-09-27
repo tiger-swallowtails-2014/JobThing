c6201d095723d6f773941e7b5a629383caa528b3
Alisa Tatarinova <alisatatarinova@gmail.com>
2014-09-27 15:48:50 -0700

    Added basic CRUD contacts controller - Also, fixed bug where user can enter blank phone number for a contact and it'll save

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,66 @@
 class ContactsController < ApplicationController
+  before_filter :load_jobthing, :load_user
+  before_filter :load_contact, :except => [:index, :new, :create]
 
   def index
+    @contacts = @jobthing.contacts
   end
+
+  def new 
+    @contact = Contact.new
+     render partial: 'form' 
+  end
+
+  def create
+    p "Contact Params"
+    p contact_params
+    @contact = Contact.new(contact_params)
+    p @contact
+    if @contact.save
+      p "contact created!"
+      @jobthing.contacts << @contact
+      redirect_to user_jobthing_contacts_path
+    else
+      redirect_to user_jobthing_contacts_path
+    end
+  end
+
+  def update
+    @contact.update_attributes(contact_params)
+    redirect_to user_jobthing_contacts_path
+  end
+
+  def show 
+  end
+
+  def destroy
+    @contact.destroy
+    redirect_to user_jobthing_contacts_path
+
+  end
+
+  def edit
+    render partial: 'form'
+  end
+
+
+  private 
+  def load_jobthing 
+    @jobthing = Jobthing.find(params[:jobthing_id])
+  end
+
+  def load_user 
+    @user = User.find(params[:user_id])
+  end
+
+  def load_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :phone_number, :email)
+  end
+
+
 
 end
