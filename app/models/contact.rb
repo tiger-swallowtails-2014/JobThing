5eda3d56bf82a1342196b95ab7d0d2022a981a2f
Alisa Tatarinova <alisatatarinova@gmail.com>
2014-09-26 17:01:16 -0700

    Added a phone number validation for contacts(works for US numbers)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
+  validates_format_of  :phone_number, with: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/
 	belongs_to :jobthing
 end
