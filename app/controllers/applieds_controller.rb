d0150323bcfbc2f49dc01742051a678b830f85ce
William Pangestu <pangestu.william@gmail.com>
2014-09-27 19:03:06 -0700

    Create applied controller

--- app/controllers/applieds_controller.rb
+++ app/controllers/applieds_controller.rb
@@ -1,6 +1,22 @@
 class AppliedsController < ApplicationController
 
-  def index
+  def create
+    @user = User.find(params[:user_id])
+    @jobthing = Jobthing.find(params[:jobthing_id])
+    @applied = Applied.create()
+    if @applied.save
+      @jobthing.applied = @applied
+      redirect_to user_path(@user)
+    else
+      redirect_to user_path(@user)
+    end
+
+  end
+
+  def destroy
+    @applied = Applied.find(params[:id])
+    @applied.destroy
+    redirect_to user_path(@user)
   end
 
 end
