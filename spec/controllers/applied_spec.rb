c9e19605bc459765578f7931f4e798c486908e0c
William Pangestu <pangestu.william@gmail.com>
2014-09-27 20:55:32 -0700

    add some tests to the applied controller spec

--- spec/controllers/applied_spec.rb
+++ spec/controllers/applied_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
-describe AppliedsController do
+describe AppliedsController, type: :controller do
+  # let!(:applied) {FactoryGirl.create :applied}
 
   describe "Post create" do
     it "should create a new applied job with successfull response" do
@@ -16,6 +17,12 @@ describe AppliedsController do
         should redirect_to controller: users, action: :show
       }
     end
+
+    # it "should increase the number of applied job" do
+    #   expect {
+    #     post :create
+    #   }.to change{Applied.count}
+    # end
 
   end
 
