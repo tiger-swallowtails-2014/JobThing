5dafcb0f66c3ff039c6639586b55e932c60b5a6c
Alisa Tatarinova <alisatatarinova@gmail.com>
2014-09-28 17:39:13 -0700

    Created basic form for log in

--- app/assets/javascripts/homepage/homepage.js
+++ app/assets/javascripts/homepage/homepage.js
@@ -2,12 +2,15 @@ var navBar = {
 
   bindEvents: function(){
     $('.job-thing-example').on("click", this.renderExample)
+    $('.sign-in-btn').on("click", this.signIn)
   },
   renderExample: function(){
     console.log("Rendering an example!")
     $('.titlebar').hide()
     $('.example-div').hide()
-
+  },
+  signIn: function(){
+    $('.titlebar').hide()
   }
 
 }
---
+++ app/assets/javascripts/homepage/login.js
@@ -0,0 +1,9 @@
+(function(){
+  var app = angular.module('mainPage',[]);
+    app.controller("LogInController", function(){})
+
+
+
+
+
+})();
