a2682e6c6601ee012083c7208d33aa92120e813d
myeung58 <myeung58gmail.com>
2014-09-29 11:58:22 -0700

    create several helper methods for job page

--- app/helpers/jobpage_helper.rb
+++ app/helpers/jobpage_helper.rb
@@ -1,5 +1,29 @@
 module JobpageHelper
 
+  def applied?
+    @jobthing.applied ? true : false
+  end
+
+  def interviews?
+    @jobthing.interviews ? true : false
+  end
+
+  def miscjobthings?
+    @jobthing.miscjobthings ? true : false
+  end
+
+  def outcome?
+    @jobthing.outcome ? true : false
+  end
+
+  def notes?
+    @jobthing.notes ? true : false
+  end
+
+  def contacts?
+    @jobthing.contacts ? true : false
+  end
+
   def day(date)
     Date.parse(date.to_date.to_s).strftime("%d")
   end
