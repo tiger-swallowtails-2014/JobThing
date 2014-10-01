08c96f6bc1b71decda0f4d4b92eeafa36d233a18
Alisa Tatarinova <alisatatarinova@gmail.com>
2014-10-01 16:14:41 -0700

    Fixed User model spec tests to make up for the new database constraints. ActiveRecord was returning errors due to setting fields to nil when validating the uniqueness of other elements

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,16 @@ describe User do
     it { should validate_presence_of :username }
     it { should validate_presence_of :email }
     it { should validate_presence_of :password }
-    it { should validate_uniqueness_of :username }
-    it { should validate_uniqueness_of :email }
     it { should_not allow_value("This is not an email").for(:email)}
     it { should allow_value("alisatatarinova@gmail.com").for(:email)}
+    it "should validate the uniqueness of a username" do
+       create(:user)
+       should validate_uniqueness_of(:username)
+    end
+    it "should validate the uniqueness of an email" do
+       create(:user)
+       should validate_uniqueness_of(:email)
+    end
     it "should create a valid user when all fields are valid" do
       user = build(:user)
       expect(user).to be_valid
